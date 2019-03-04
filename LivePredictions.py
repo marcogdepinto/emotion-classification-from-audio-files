8b77f425f391f43430e6a3e845e57952f78c9f3f
Arun Poochelvan <arunselvabio@gmail.com>
2019-03-05 00:59:10 +0530

    Update LivePredictions.py

--- LivePredictions.py
+++ LivePredictions.py
@@ -1,6 +1,8 @@
 import keras
 import numpy as np
 import librosa
+import pyaudio
+import wave
 
 class livePredictions:
 
@@ -12,19 +14,18 @@ class livePredictions:
     def load_model(self):
         '''
         I am here to load you model.
-
         :param path: path to your h5 model.
         :return: summary of the model with the .summary() function.
-
         '''
         self.loaded_model = keras.models.load_model(self.path)
-        return self.loaded_model.summary()
+        return self.loaded_model
 
     def makepredictions(self):
         '''
         I am here to process the files and create your features.
         '''
         data, sampling_rate = librosa.load(self.file)
+        print(data)
         mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
         x = np.expand_dims(mfccs, axis=2)
         x = np.expand_dims(x, axis=0)
@@ -64,9 +65,35 @@ class livePredictions:
 
 # Here you can replace path and file with the path of your model and of the file from the RAVDESS dataset you want to use for the prediction,
 # Below, I have used a neutral file: the prediction made is neutral.
+while True:
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 22050
+    CHUNK = 1024
+    RECORD_SECONDS = 4
+    WAVE_OUTPUT_FILENAME = "file.wav"
+    audio = pyaudio.PyAudio()
+ 
+    # start Recording
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+    #print ("recording...")
+    frames = []
 
-pred = livePredictions(path='/Users/marcogdepinto/Desktop/Ravdess_V2/Emotion_Voice_Detection_Model.h5',
-                       file='/Users/marcogdepinto/Desktop/Ravdess_V2/01-01-01-01-01-01-01.wav')
-
-pred.load_model()
-pred.makepredictions()
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    #print ("finished recording")
+    
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
+ 
+    pred = livePredictions(path=r'C:\Users\apoochelvan\Downloads\Emotion_Voice_Detection_Model (3).h5',
+                       file=r'C:\Users\apoochelvan\audio\file.wav')
+    pred.load_model()
+    pred.makepredictions()
