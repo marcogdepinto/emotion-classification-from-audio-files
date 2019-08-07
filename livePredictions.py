489efc179719d3e4601179ebddef28fb6ea09df2
Marco Giuseppe de Pinto <marcog.depinto@gmail.com>
2019-08-07 22:34:04 +0200

    Update livePredictions.py

--- livePredictions.py
+++ livePredictions.py
@@ -11,12 +11,9 @@ class livePredictions:
 
     def load_model(self):
         '''
-
         I am here to load you model.
-
         :param path: path to your h5 model.
         :return: summary of the model with the .summary() function.
-
         '''
         self.loaded_model = keras.models.load_model(self.path)
         return self.loaded_model.summary()
@@ -25,7 +22,6 @@ class livePredictions:
         '''
         I am here to process the files and create your features.
         '''
-
         data, sampling_rate = librosa.load(self.file)
         mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
         x = np.expand_dims(mfccs, axis=2)
@@ -39,28 +35,28 @@ class livePredictions:
         '''
         self.pred  = pred
 
-        if pred == 1:
+        if pred == 0:
             pred = "neutral"
             return pred
-        elif pred == 2:
+        elif pred == 1:
             pred = "calm"
             return pred
-        elif pred == 3:
+        elif pred == 2:
             pred = "happy"
             return pred
-        elif pred == 4:
+        elif pred == 3:
             pred = "sad"
             return pred
-        elif pred == 5:
+        elif pred == 4:
             pred = "angry"
             return pred
-        elif pred == 6:
+        elif pred == 5:
             pred = "fearful"
             return pred
-        elif pred == 7:
+        elif pred == 6:
             pred = "disgust"
             return pred
-        elif pred == 8:
+        elif pred == 7:
             pred = "surprised"
             return pred
 
