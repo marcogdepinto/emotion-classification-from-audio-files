b92182a0b36d532de446e5764a77cc71ed12b4e0
Marco Giuseppe de Pinto <marcog.depinto@gmail.com>
2019-11-24 13:56:55 +0100

    Refactoring

--- livePredictions.py
+++ livePredictions.py
@@ -1,27 +1,36 @@
+"""
+This file can be used to try a live prediction. 
+"""
+
 import keras
 import numpy as np
 import librosa
 
 class livePredictions:
+    """
+    Main class of the application.
+    """
 
     def __init__(self, path, file):
-
+        """
+        Init method is used to initialize the main parameters.
+        """
         self.path = path
         self.file = file
 
     def load_model(self):
-        '''
-        I am here to load you model.
+        """
+        Method to load the chosen model.
         :param path: path to your h5 model.
         :return: summary of the model with the .summary() function.
-        '''
+        """
         self.loaded_model = keras.models.load_model(self.path)
         return self.loaded_model.summary()
 
     def makepredictions(self):
-        '''
-        I am here to process the files and create your features.
-        '''
+        """
+        Method to process the files and create your features.
+        """
         data, sampling_rate = librosa.load(self.file)
         mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
         x = np.expand_dims(mfccs, axis=2)
@@ -29,38 +38,28 @@ class livePredictions:
         predictions = self.loaded_model.predict_classes(x)
         print( "Prediction is", " ", self.convertclasstoemotion(predictions))
 
-    def convertclasstoemotion(self, pred):
-        '''
-        I am here to convert the predictions (int) into human readable strings.
-        '''
-        self.pred  = pred
+    @staticmethod
+    def convertclasstoemotion(pred):
+        """
+        Method to convert the predictions (int) into human readable strings.
+        """
+        
+        label_conversion = {'0': 'neutral',
+                            '1': 'calm',
+                            '2': 'happy',
+                            '3': 'sad',
+                            '4': 'angry',
+                            '5': 'fearful',
+                            '6': 'disgust',
+                            '7': 'surprised'}
 
-        if pred == 0:
-            pred = "neutral"
-            return pred
-        elif pred == 1:
-            pred = "calm"
-            return pred
-        elif pred == 2:
-            pred = "happy"
-            return pred
-        elif pred == 3:
-            pred = "sad"
-            return pred
-        elif pred == 4:
-            pred = "angry"
-            return pred
-        elif pred == 5:
-            pred = "fearful"
-            return pred
-        elif pred == 6:
-            pred = "disgust"
-            return pred
-        elif pred == 7:
-            pred = "surprised"
-            return pred
+        for key, value in label_conversion.items():
+            if int(key) == pred:
+                label = value
+        return label
 
-# Here you can replace path and file with the path of your model and of the file from the RAVDESS dataset you want to use for the prediction,
+# Here you can replace path and file with the path of your model and of the file 
+#from the RAVDESS dataset you want to use for the prediction,
 # Below, I have used a neutral file: the prediction made is neutral.
 
 pred = livePredictions(path='/Users/marcogdepinto/Desktop/Ravdess_V2/Emotion_Voice_Detection_Model.h5',
