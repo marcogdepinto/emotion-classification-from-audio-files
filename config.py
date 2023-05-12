e593a85a4b8fd3f9cd85022cc10fa0e9f63888b4
SReza SMohseni <srezas.2017@gmail.com>
2023-05-12 18:37:29 +0330

    Fixed config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ import os
 
 working_dir_path = pathlib.Path().absolute()
 
-TRAINING_FILES_PATH = os.path.join(str(working_dir_path) + 'features')
-SAVE_DIR_PATH = os.path.join(str(working_dir_path) + 'joblib_features')
-MODEL_DIR_PATH = os.path.join(str(working_dir_path) + 'model')
-TESS_ORIGINAL_FOLDER_PATH = os.path.join(str(working_dir_path) + 'TESS_Toronto_emotional_speech_set_data')
-EXAMPLES_PATH = os.path.join(str(working_dir_path) + 'examples')
+TRAINING_FILES_PATH = os.path.join(str(working_dir_path), 'features')
+SAVE_DIR_PATH = os.path.join(str(working_dir_path), 'joblib_features')
+MODEL_DIR_PATH = os.path.join(str(working_dir_path), 'model')
+TESS_ORIGINAL_FOLDER_PATH = os.path.join(str(working_dir_path), 'TESS_Toronto_emotional_speech_set_data')
+EXAMPLES_PATH = os.path.join(str(working_dir_path), 'examples')
